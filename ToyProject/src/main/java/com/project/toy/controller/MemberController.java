8d9d44d9b237ab706507cf9451b3c864cb9eb6ec
choi0dev <waypop@naver.com>
2021-07-04 23:17:11 +0900

    로그인, 로그아웃 구현

--- ToyProject/src/main/java/com/project/toy/controller/MemberController.java
+++ ToyProject/src/main/java/com/project/toy/controller/MemberController.java
@@ -1,15 +1,46 @@
 package com.project.toy.controller;
 
 import javax.inject.Inject;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.project.toy.service.MemberService;
+import com.project.toy.vo.MemberVO;
 
 @Controller
 public class MemberController {
 	@Inject
 	MemberService service;
 	
+	@RequestMapping("/loginForm")
+	public String loginForm() {
+		return "member/loginForm";
+	}
+	
+	@RequestMapping(value="/loginOk", method=RequestMethod.POST)
+	public ModelAndView loginOk(MemberVO vo, HttpSession session) {
+		ModelAndView mav=new ModelAndView();
+		String logId=service.loginCheck(vo).getUserid();
+		
+		if(logId!=null) {
+			session.setAttribute("logId", logId);
+			mav.setViewName("redirect:/");
+		}else {
+			mav.setViewName("redirect:loginForm");
+		}
+		
+		return mav;
+	}
+	
+	@RequestMapping("/logoutOk")
+	public String logouOk(HttpSession session) {
+		session.invalidate();
+		
+		return "home";
+	}
 	
 }
--- ToyProject/src/main/webapp/WEB-INF/views/home.jsp
+++ ToyProject/src/main/webapp/WEB-INF/views/home.jsp
@@ -5,10 +5,14 @@
 	<title>Home</title>
 </head>
 <body>
-<h1>
-	Hello world!  
-</h1>
+	<c:if test="${logId==null}">
+		<a href="loginForm">로그인</a>
+	</c:if>
+	<c:if test="${logId!=null}">
+		${logId}님
+		<a href="logoutOk">로그아웃</a>
+	</c:if>
+	게시판
 
-<P>반갑습니다.</P>
 </body>
 </html>
